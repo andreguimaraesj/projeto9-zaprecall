d03a1df0d6c16ed7f3c7444fec86064b64bae330
andreguimaraesj <dev.detto@gmail.com>
2023-05-17 19:26:04 -0300

    styled components

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Main from "./components/Main";
+import GlobalStyle from "./GlobalStyle";
+import React from "react";
+import FrontPage from "./components/FrontPage";
+import styled from "styled-components";
+
+function App() {
+  const [clickStart, setClickStart] = React.useState(false)
+
+  return (
+    <>
+      <GlobalStyle />
+
+    <FrontVisibility clickStart={clickStart}>
+        <FrontPage setClickStart={setClickStart} />
+    </FrontVisibility>
+
+      <MainVisibility clickStart={clickStart}>
+         <Main />
+      </MainVisibility>
+    </>
+  );
+} 
+
+export default App;
